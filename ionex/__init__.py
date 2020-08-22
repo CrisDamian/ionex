c25ff3984a1bd72b8e8bc01e871bbded2e303bdc
Cristian Damian <CrisDamian@users.noreply.github.com>
2020-08-22 19:05:16 +0300

    Google translate

--- ionex/__init__.py
+++ ionex/__init__.py
@@ -10,21 +10,22 @@ def _get_version_type(line):
 
 
 def reader(file):
-    """Возвращает читалку файла в формате IONEX.
-    Читалка - итерируемый объект, на каждой итерации возвращает экземпляр
-    ``ionex_map.IonexMap`` очередной карты, прочитанной из файла.
-
-    :type file: str | file-object
-    :param file: Путь к файлу IONEX или объект файла.
-
-    :raises IONEXError:
-        Если неизвестный тип или версия переданного файла.
-
-    :raises IONEXUnexpectedEnd:
-        Неполный файл.
-
-    :raises IONEXMapError:
-        Если возникли ошибки при обработке карты.
+    """Returns the file reader in IONEX format.
+     The reader is an iterable object, at each iteration it returns an instance
+     
+     `` ionex_map.IonexMap`` next map read from file.
+     : type file: str | file-object
+     
+     : param file: Path to the IONEX file or file object.
+ 
+     : raises IONEXError:
+        If the type or version of the uploaded file is unknown.
+     
+     : raises IONEXUnexpectedEnd:
+         Incomplete file.
+     
+     : raises IONEXMapError:
+         If there are errors while processing the card.
     """
     readers = {
         1.0: IonexV1,
